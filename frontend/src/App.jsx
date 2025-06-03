c90ac808ad205e7a47ffafa9905fa131560f6df7
nirrie <97035721@st.deltion.nl>
2025-06-03 14:30:59 +0200

    add DashboardPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { route } from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { StudentDashboard } from './components/StudentDashboard'
+import { TeacherDashboard } from './components/TeacherDashboard'
+
 import './App.css'
 
 function App() {
