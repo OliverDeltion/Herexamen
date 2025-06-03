1e0c3aa5316240898a05fa94647234a9b3b1112c
Jeroen038 <97095929@st.deltion.nl>
2025-06-03 15:07:49 +0200

    Remove Tailwind

---
+++ frontend/src/components/common/NavigationBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<div className="navbar__container">
+				<div className="navbar__brand">AARDATA</div>
+				<ul className="navbar__menu">
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Overzicht</a>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
