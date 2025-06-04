4f4fba2c1f9f599104e7f39b7f0d8dbd0406a6a2
Nirmin <97035721@st.deltion.nl>
2025-06-04 14:54:02 +0200

    update placeholder content

--- frontend/src/components/dashboard/TeacherDashboard.jsx
+++ frontend/src/components/dashboard/TeacherDashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavigationBar from "../common/NavigationBar";
 
 const TeacherDashboard = () => {
-	return <div style={{ padding: "2rem" }}>Teacher Dashboard (binnenkort)</div>;
+	return <div style={{ padding: "2rem" }}>Docenten Dashboard (binnenkort)</div>;
 };
 
 export default TeacherDashboard;
