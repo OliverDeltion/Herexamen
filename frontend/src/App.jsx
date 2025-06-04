6c12850c0e6f6da89d10d8a7a34f055098070304
Nirmin <97035721@st.deltion.nl>
2025-06-04 13:30:30 +0200

    remove uneccesary imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 //import "./styles/global.css";
-import { route } from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavigationBar from "./components/common/NavigationBar";
-import Footer from "./components/common/Footer";
-import SpreadsheetButton from "./components/common/SpreadsheetButton";
-import FilterButton from "./components/common/FilterButton";
-import DownloadButton from "./components/common/DownloadButton";
 import DashboardPage from "./pages/DashboardPage";
 
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
 
 	return (
 		<Router>
