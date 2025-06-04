57fa039e6cb545aca2bebcbe912990b7370302a6
Nirmin <97035721@st.deltion.nl>
2025-06-04 13:29:55 +0200

    Add temporary content to test modal

--- frontend/src/components/dashboard/StudentDashboard.jsx
+++ frontend/src/components/dashboard/StudentDashboard.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import NavigationBar from "../common/NavigationBar";
 
 const StudentDashboard = () => {
-	return <div style={{ padding: "2rem" }}>Student Dashboard (binnenkort)</div>;
+	return <div style={{
+		padding: "2rem",
+		backgroundColor: "#ffffff",
+		border: "2px solid #3366cc",
+		borderRadius: "8px",
+		color: "#000",
+	}}
+	>
+		<h2>Studenten Dashboard (binnenkort)</h2>
+		<p>Modal werkt!</p>
+	</div>
+	
 };
 
 export default StudentDashboard;
