a15e2d422d63a4453ab4d42a1bcb6f8764eb31c9
Jeroen038 <97095929@st.deltion.nl>
2025-06-05 14:01:26 +0200

    Fix

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -2,72 +2,71 @@ import React, { useState } from "react";
 import Footer from "../components/common/Footer";
 import StudentDashboard from "../components/dashboard/StudentDashboard";
 import TeacherDashboard from "../components/dashboard/TeacherDashboard";
+import Modal from "../components/common/Modal";
 
 //Styles
 import "../styles/global.css";
 import "../App.css";
 
 const DashboardPage = () => {
-    const [selectedRole, setSelectedRole] = useState(null);
-    console.log("Selected Role:", selectedRole);
+	const [selectedRole, setSelectedRole] = useState(null);
+	console.log("Selected Role:", selectedRole);
 
-    const handleSelectedRole = (role) => {
-        setSelectedRole(role);
-    };
+	const handleSelectedRole = (role) => {
+		setSelectedRole(role);
+	};
 
-    const handleBack = () => {
-        setSelectedRole(null);
-    };
+	const handleBack = () => {
+		setSelectedRole(null);
+	};
 
 	return (
-		<>	
+		<>
 			<div className="dashboard-container">
 				<header className="dashboard-header">
 					<h1 className="dashboard-title">Dashboard AARdata</h1>
-					</header>
+				</header>
 				{selectedRole === null && (
 					<>
 						<div className="dashboard-card">
-						<h2 className="dashboard-heading">Selecteer account</h2>
-						<div className="dashboard-buttons">
-						<button onClick={() => handleSelectedRole("student")} className="btn student-btn">
-							Student
-						</button>
-						<button onClick={() => handleSelectedRole("docent")} className="btn docent-btn">
-							Docent
+							<h2 className="dashboard-heading">Selecteer account</h2>
+							<div className="dashboard-buttons">
+								<button onClick={() => handleSelectedRole("student")} className="btn student-btn">
+									Student
 								</button>
-								</div>
+								<button onClick={() => handleSelectedRole("docent")} className="btn docent-btn">
+									Docent
+								</button>
+							</div>
 						</div>
 					</>
 				)}
 				<Modal isOpen={!!selectedRole} onClose={handleBack}>
 					<div className="dashboard-modal-content">
-				{selectedRole === "student" && (
+						{selectedRole === "student" && (
 							<>
-							<p>Student dashboard</p>
-						<button onClick={handleBack} className="student-terug">
-							Terug
-						</button>
-						<StudentDashboard />
+								<p>Student dashboard</p>
+								<button onClick={handleBack} className="student-terug">
+									Terug
+								</button>
+								<StudentDashboard />
 							</>
-				)}
+						)}
 
-				{selectedRole === "docent" && (
+						{selectedRole === "docent" && (
 							<>
-						<p>Docenten dashboard</p>
-						<button onClick={handleBack} className="docent-terug">
-							Terug
-						</button>
-						<TeacherDashboard />
+								<p>Docenten dashboard</p>
+								<button onClick={handleBack} className="docent-terug">
+									Terug
+								</button>
+								<TeacherDashboard />
 							</>
 						)}
 					</div>
 				</Modal>
 			</div>
 			<Footer />
-		
 		</>
-		
 	);
 };
 
