db0e933671c8ce3f0a411a137837c45d4531d516
TeunPleiter <97097572@st.deltion.nl>
2025-06-10 10:33:35 +0200

    Volgorde aangepast

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -5,23 +5,16 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<div className="footer__section">
-				<a href="#">Student</a>
-				<a href="#">Docent</a>
-				<a href="#">Mozartlaan 15</a>
-				<a href="#">8031 AA Zwolle</a>
-				<a href="#">+31 038 850 3000</a>
-			</div>
-
-			<div className="footer__section">
-				<a href="#">Groepen</a>
-				<a href="#">Studenten</a>
-				<a href="#">Importeer Stylesheet</a>
-			</div>
-
-			<div className="footer__section">
-                <a href="/Privacybeleid.pdf" target="_blank" rel="noopener noreferrer">Privacybeleid</a>
+				<a href="/Privacybeleid.pdf" target="_blank" rel="noopener noreferrer">Privacybeleid</a>
                 <a href="/Gebruiksvoorwaarden.pdf" target="_blank" rel="noopener noreferrer">Gebruiksvoorwaarden</a>
-                <a href="#">Cookies</a>
+			</div>
+
+			<div className="footer__section">
+				<a href="tel:+310388503000">+31 038 850 3000</a>
+			</div>
+
+			<div className="footer__section">
+				<a href="https://www.google.com/maps/place/Mozartlaan+15,+8031+AA+Zwolle">Mozartlaan 15, 8031 AA Zwolle</a>
             </div>
 		</footer>
 	);
